96b01069bef84f9eb27fdf0574145418018d5c03
dmadd1996 <94644880+dmadd1996@users.noreply.github.com>
2022-04-20 16:53:57 -0500

    small edits

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,20 @@
 var cityInput = document.getElementById('cityInput')
 var citySubmit = document.getElementById('citySubmit')
 
+var mainTemp = document.getElementById('mainTemp')
+var mainWind = document.getElementById('mainWind')
+var mainHumid = document.getElementById('mainHumid')
+var mainUV = document.getElementById('mainUV')
+
 citySubmit.addEventListener('click', function () {
     var cityName = cityInput.value
 
-    var apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=2c9d646cbf2ee524b60b45419e486291`
+    var cityAPI = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=2c9d646cbf2ee524b60b45419e486291`
 
-    fetch(apiUrl).then(function (response) {
+    fetch(cityAPI).then(function (response) {
         response.json().then(function (data) {
             console.log(data);
         });
     });
 
-})
+});
